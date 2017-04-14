224e73ac720d7acf9b69de47194b4924ce3ec29a
Andi Rady Kurniawan <andirady@localhost.localdomain>
2017-04-14 22:25:58 +0800

    Allow to change paned separator color. Fix bogus new split size

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <glib.h>
-#include <gtk/gtk.h>
-#include <vte/vte.h>
+#include "config.h"
+
+#define PALETTE_SIZE 16
+
+GdkColor bg;
+GdkColor fg;
+GdkColor palette[PALETTE_SIZE];
+PangoFontDescription *font_desc;
 
 enum SplitOrientation {
     HORIZONTAL,
@@ -49,17 +52,23 @@ static void child_exited(VteTerminal *term, gint status, gpointer data)
 
 static void on_terminal_window_title_changed(VteTerminal *term, gpointer data)
 {
-    gboolean has_focus;
-    g_object_get(term, "has-focus", &has_focus, NULL);
-    if (has_focus)
-        gtk_window_set_title(GTK_WINDOW(data), vte_terminal_get_window_title(term));
+    gtk_window_set_title(GTK_WINDOW(data), vte_terminal_get_window_title(term));
+}
+
+static void on_current_directory_uri_changed(VteTerminal *term, gpointer data)
+{
+    g_print(vte_terminal_get_current_directory_uri(term));
 }
 
 static GtkWidget* new_term(GtkWidget *main_window, GError **error)
 {
     char *argv[] = {"/usr/bin/bash", NULL};
     GPid pid;
+
     GtkWidget *vte = vte_terminal_new();
+    vte_terminal_set_color_foreground(VTE_TERMINAL(vte), &fg);
+    vte_terminal_set_color_background(VTE_TERMINAL(vte), &bg);
+    //vte_terminal_set_colors(VTE_TERMINAL(vte), &fg, &bg, palette, PALETTE_SIZE);
     vte_terminal_fork_command_full(VTE_TERMINAL(vte),
                                    VTE_PTY_DEFAULT,
                                    getenv("HOME"),
@@ -70,6 +79,7 @@ static GtkWidget* new_term(GtkWidget *main_window, GError **error)
                                    NULL,
                                    &pid,
                                    error);
+
     g_signal_connect(vte,
                      "child-exited",
                      G_CALLBACK(child_exited),
@@ -78,28 +88,64 @@ static GtkWidget* new_term(GtkWidget *main_window, GError **error)
                      "window-title-changed",
                      G_CALLBACK(on_terminal_window_title_changed),
                      main_window);
+    /*
+    g_signal_connect(vte,
+                     "current-directory-uri-changed",
+                     G_CALLBACK(on_current_directory_uri_changed),
+                     NULL);
+    */
     return vte;
+}
+
+GtkCssProvider *css = NULL;
+
+void modify_css(GtkWidget *widget, const gchar *class) {
+    GError *error = NULL;
+    if (css == NULL) {
+        css = gtk_css_provider_new();
+        gtk_css_provider_load_from_data(css,
+                                        "paned.horizontal separator {"
+                                        "  background: #d5d5d0; "
+                                        "}"
+                                        "paned.vertical separator {"
+                                        //"  background: #fa2772; "
+                                        "  background: #93bd18; "
+                                        //"  background: #65cdde; "
+                                        "}\0",
+                                        -1,
+                                        &error);
+        if (error != NULL) {
+            fprintf(stderr, "Error: %s\n", error->message);
+            g_error_free(error);
+            return;
+        }
+    }
+    GtkStyleContext *ctx = gtk_widget_get_style_context(widget);
+    gtk_style_context_add_provider(ctx,
+                                   GTK_STYLE_PROVIDER(css),
+                                   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    gtk_style_context_add_class(ctx, class);
 }
 
 static void split(GtkWidget *main_window, GtkOrientation orientation)
 {
     GtkWidget *focus = gtk_container_get_focus_child(GTK_CONTAINER(main_window));
-    if (focus == NULL)
-    {
+    if (focus == NULL) {
         fprintf(stderr, "Error: No focus child.\n");
         return;
     }
-    if (GTK_IS_PANED(focus))
-    {
+    if (GTK_IS_PANED(focus)) {
         while (!VTE_IS_TERMINAL(focus))
             focus = gtk_container_get_focus_child(GTK_CONTAINER(focus));
     }
-    if (VTE_IS_TERMINAL(focus))
-    {
+    if (VTE_IS_TERMINAL(focus)) {
         GtkWidget *vte;
-        GtkWidget *parent = gtk_widget_get_parent(focus);
+        GtkAllocation alloc;
+        gtk_widget_get_allocation(focus, &alloc);
 
+        GtkWidget *parent = gtk_widget_get_parent(focus);
         GtkWidget *paned = gtk_paned_new(orientation);
+
         g_object_ref(focus);
         gtk_container_remove(GTK_CONTAINER(parent), focus);
         gtk_container_add(GTK_CONTAINER(parent), paned);
@@ -110,12 +156,13 @@ static void split(GtkWidget *main_window, GtkOrientation orientation)
         gtk_paned_add2(GTK_PANED(paned), vte);
         gtk_widget_show_all(paned);
 
-        GtkAllocation alloc;
-        gtk_widget_get_allocation(parent, &alloc);
-        if (orientation == GTK_ORIENTATION_HORIZONTAL)
+        if (orientation == GTK_ORIENTATION_HORIZONTAL) {
+            modify_css(paned, "horizontal");
             gtk_paned_set_position(GTK_PANED(paned), alloc.width / 2);
-        else
+        } else {
+            modify_css(paned, "vertical");
             gtk_paned_set_position(GTK_PANED(paned), alloc.height / 2);
+        }
 
         g_object_set(vte, "has-focus", TRUE, NULL);
     }
@@ -135,6 +182,48 @@ static gboolean split_vertical(GtkAccelGroup *sender, gpointer data)
     return TRUE;
 }
 
+static gboolean copy_clipboard(GtkAccelGroup *sender, gpointer data)
+{
+    GtkWidget *focus = gtk_container_get_focus_child(GTK_CONTAINER(data));
+    while (!VTE_IS_TERMINAL(focus)) {
+        focus = gtk_container_get_focus_child(GTK_CONTAINER(focus));
+    }
+    vte_terminal_copy_clipboard(VTE_TERMINAL(focus));
+
+    return TRUE;
+}
+
+static gboolean paste_clipboard(GtkAccelGroup *sender, gpointer data)
+{
+    GtkWidget *focus = gtk_container_get_focus_child(GTK_CONTAINER(data));
+    while (!VTE_IS_TERMINAL(focus)) {
+        focus = gtk_container_get_focus_child(GTK_CONTAINER(focus));
+    }
+    vte_terminal_paste_clipboard(VTE_TERMINAL(focus));
+
+    return TRUE;
+}
+
+static void setup_palette()
+{ 
+    gdk_color_parse("#1B1D1E", &palette[0]);
+    gdk_color_parse("#F92672", &palette[1]);
+    gdk_color_parse("#82B414", &palette[2]);
+    gdk_color_parse("#FD971F", &palette[3]);
+    gdk_color_parse("#56C2D6", &palette[4]);
+    gdk_color_parse("#8C54FE", &palette[5]);
+    gdk_color_parse("#465457", &palette[6]);
+    gdk_color_parse("#CCCCC6", &palette[7]);
+    gdk_color_parse("#505354", &palette[8]);
+    gdk_color_parse("#FF5995", &palette[9]);
+    gdk_color_parse("#B6E354", &palette[10]);
+    gdk_color_parse("#FEED6C", &palette[11]);
+    gdk_color_parse("#8CEDFF", &palette[12]);
+    gdk_color_parse("#9E6FFE", &palette[13]);
+    gdk_color_parse("#899CA1", &palette[14]);
+    gdk_color_parse("#F8F8F2", &palette[15]);
+}
+
 int main(int argc, char *argv[])
 {
     GtkWidget *win;
@@ -145,9 +234,15 @@ int main(int argc, char *argv[])
 
     gtk_init(&argc, &argv);
 
+    // Settings
+    font_desc = pango_font_description_from_string(FONT_SPEC);
+    gdk_color_parse(BACKGROUND_COLOR, &bg);
+    gdk_color_parse(FOREGROUND_COLOR, &fg);
+    //setup_palette();
+
     win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(win), 600, 400);
-    gtk_widget_show(win);
+    gtk_widget_override_font(win, font_desc);
     g_signal_connect(G_OBJECT(win), "destroy", G_CALLBACK(on_destroy), NULL);
 
     accel_group = gtk_accel_group_new();
@@ -161,6 +256,18 @@ int main(int argc, char *argv[])
     gtk_accel_group_connect(accel_group,
                             GDK_KEY_underscore,
                             GDK_CONTROL_MASK,
+                            0,
+                            closure);
+    closure = g_cclosure_new(G_CALLBACK(copy_clipboard), win, 0);
+    gtk_accel_group_connect(accel_group,
+                            GDK_KEY_c,
+                            GDK_CONTROL_MASK | GDK_SHIFT_MASK,
+                            0,
+                            closure);
+    closure = g_cclosure_new(G_CALLBACK(paste_clipboard), win, 0);
+    gtk_accel_group_connect(accel_group,
+                            GDK_KEY_v,
+                            GDK_CONTROL_MASK | GDK_SHIFT_MASK,
                             0,
                             closure);
     gtk_window_add_accel_group(GTK_WINDOW(win), accel_group);
@@ -178,5 +285,6 @@ int main(int argc, char *argv[])
         g_object_set(vte, "has-focus", TRUE, NULL);
     }
 
+    gtk_widget_show_all(win);
     gtk_main();
 }
